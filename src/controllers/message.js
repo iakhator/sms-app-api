230d5663450124bd27220057b50511fb15095909
Akhator Itua <iakhator@gmail.com>
2018-11-10 15:55:03 +0100

    send sms

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -26,4 +26,71 @@ module.exports = {
       })
   },
 
+  sendSms(req, res) {
+    if (req.body.sender === '') {
+      return res.status(400).send({
+        message: 'Sender Field must not be empty'
+      })
+    }
+    if (req.body.reciever === '') {
+      return res.status(400).send({
+        message: 'Reciever Field must not be empty'
+      })
+    }
+
+    if (req.body.sms === '') {
+      return res.status(400).send({
+        message: 'Sms field must not be empty'
+      })
+    }
+
+    if (req.body.sender === req.body.reciever) {
+      return res.status(400).send({
+        message: 'sender and reciever\'s phone number must be unique'
+      })
+    }
+    return Contact
+      .find({
+        where: {
+          contact_phone: req.body.sender
+        }
+      })
+      .then(sender => {
+        if (!sender) {
+          res.status(404).send({
+            message: 'Sender not Found'
+          })
+        }
+        let users = [sender]
+        return Contact
+          .find({
+            where: {
+              contact_phone: req.body.reciever
+            }
+          }).then(reciever => {
+            users.push(reciever)
+            return users
+          })
+      }).then(users => {
+        if (!users) {
+          return res.status(404).send({
+            message: 'Users Not Found'
+          })
+        }
+        return Message.create({
+            sms: req.body.sms,
+            sender: users[0].id
+          })
+          .then(message => {
+            message.setContacts(users)
+            return res.status(201).send({
+              data: message,
+              message: 'sms sent successfully'
+            })
+          })
+          .catch((error) => res.status(400).send(error))
+      })
+      .catch((error) => res.status(400).send(error))
+  },
+
 }
